159c12b0229cd098ba957d25c2b7adee890e9c68
Ulisse Mini <ulisse.mini@gmail.com>
2019-04-08 12:16:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# lua + a bunch of exported go functions
+
+## Example
+```lua
+local addr = 'localhost:1337'
+local conn, err = dial(addr)
+if err ~= nil then
+  print('error: '..err)
+  return
+end
+
+print('connected to '..addr)
+
+print 'sending...'
+conn.write('foo')
+conn.write('bar from lua\n')
+
+print 'reading...'
+data, err = conn.read(1024)
+if err ~= nil then
+  print('error: '..err)
+else
+  print('got: '..data)
+end
+
+print 'closing...'
+conn.close()
+
+print 'all done!'
+```
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+
+	lua "github.com/yuin/gopher-lua"
+)
+
+func LErr(err error) lua.LValue {
+	if err == nil {
+		return lua.LNil
+	}
+
+	return lua.LString(err.Error())
+}
+
+func luaErrWrap(L *lua.LState, err error) {
+	if err != nil {
+		L.Push(lua.LString(err.Error()))
+	} else {
+		L.Push(lua.LNil)
+	}
+}
+
+type LConn struct{ net.Conn }
+
+// The write function exported to lua. returns nil or error string.
+func (c *LConn) LuaWrite(L *lua.LState) int {
+	s := L.ToString(1)
+	_, err := io.WriteString(c, s)
+	luaErrWrap(L, err)
+	return 1
+}
+
+// The close function exported to lua.
+func (c *LConn) LuaClose(L *lua.LState) int {
+	err := c.Close()
+	luaErrWrap(L, err)
+	return 1
+}
+
+// The Read function exported to lua.
+func (c *LConn) LuaRead(L *lua.LState) int {
+	bufSize := L.ToInt(1)
+	buf := make([]byte, bufSize)
+
+	n, err := c.Read(buf)
+	L.Push(lua.LString(buf[:n]))
+	L.Push(LErr(err))
+	return 2
+}
+
+func Dial(L *lua.LState) int {
+	addr := L.ToString(1)
+	rawConn, err := net.Dial("tcp", addr)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(LErr(err))
+		return 2
+	}
+	t := L.NewTable()
+	conn := LConn{rawConn}
+	t.RawSetString("write", L.NewFunction(conn.LuaWrite))
+	t.RawSetString("close", L.NewFunction(conn.LuaClose))
+	t.RawSetString("read", L.NewFunction(conn.LuaRead))
+
+	L.Push(t)
+	L.Push(lua.LNil)
+	return 2
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <file.lua>\n", os.Args[0])
+		return
+	}
+
+	L := lua.NewState()
+	defer L.Close()
+	L.SetGlobal("dial", L.NewFunction(Dial))
+
+	if err := L.DoFile(os.Args[1]); err != nil {
+		log.Print(err)
+		return
+	}
+}
